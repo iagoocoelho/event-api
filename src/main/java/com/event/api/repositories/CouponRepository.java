14bd29b8b2e1902f7d5a6532c3db7ce853b908b6
iagocoelho-deal <iago.coelho@deal.com.br>
2024-07-04 17:38:58 -0300

    add repositories

---
+++ src/main/java/com/event/api/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.event.api.repositories;
+
+import com.event.api.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
